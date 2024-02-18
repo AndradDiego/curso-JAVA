905cccbc1126f22ba463218cf6b287fbbb0086a3
AndradDiego <diegoandradedev@gmail.com>
2024-02-18 19:35:48 -0300

    Criando Calculadora de Produtos

---
+++ Ex002/Main.java
@@ -0,0 +1,15 @@
+package Ex002;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner inteiros = new Scanner(System.in);
+        int n1 = inteiros.nextInt();
+        int n2 = inteiros.nextInt();
+        int PROD = n1 * n2;
+        System.out.println("PROD = "+PROD);
+        
+    }
+}
+ 
